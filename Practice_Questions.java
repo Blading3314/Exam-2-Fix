081486ca8ff9c13203580dfcd693525f4623b2b4
Le Tom <thomascastanop@gmail.com>
2025-04-25 08:59:43 -0400

    Initial sharing of project

---
+++ Practice_Questions.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class Practice24 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Practice_Questions
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Practice24
+     */
+    public Practice_Questions()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void compareStrings()
+    { //Question 24
+    String c;  
+    String v; 
+    /*if (c == v){
+        System.out.println("Equal"); 
+    } else{
+        System.out.println("Not Equal"); 
+    }
+    }
+    //Question 25
+  public void (ArrayList <> temp){
+        int i = 0;
+        while(i >temp){
+            i++; 
+        }
+        if (i > 37.5 && temp < Collections.max) {
+            System.out.println(i+"are greater than 37.5");
+            System.out.println("Max:" +temp);
+        }
+        //Question 26 
+        for (int i = 0; i >temp && i< Collections.max; i++){
+             System.out.println("Max:" + i);
+        }
+        //Question 27
+         for (int i = 0; i >temp && i< Collections.sort; i++){
+             System.out.println("Max:" + i);
+    }
+}
+*/
+}
+}
