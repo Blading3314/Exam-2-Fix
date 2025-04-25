abe44a58fa6609ec235a2a1325b9c924ff8d9e1b
Le Tom <thomascastanop@gmail.com>
2025-04-25 09:45:20 -0400

    undone

--- Practice_Questions.java
+++ Practice_Questions.java
@@ -1,4 +1,5 @@
-
+import java.util.*;
+import java.util.Collections;
 /**
  * Write a description of class Practice24 here.
  *
@@ -9,14 +10,21 @@ public class Practice_Questions
 {
     // instance variables - replace the example below with your own
     private int x;
-
     /**
      * Constructor for objects of class Practice24
      */
     public Practice_Questions()
     {
         // initialise instance variables
-        x = 0;
+        x = 0 ;
+        ArrayList temperatures = new ArrayList <Double>();
+        temperatures.add(34.5);
+        temperatures.add(40.3);
+        temperatures.add(80.1);
+        temperatures.add(60.9);
+        temperatures.add(38.9);
+        temperatures.add(42.4);
+        //findHighestTemp(temperatures);
     }
 
     /**
@@ -25,19 +33,28 @@ public class Practice_Questions
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public void compareStrings()
+    public boolean compareStrings()
     { //Question 24
-    String c;  
-    String v; 
-    /*if (c == v){
-        System.out.println("Equal"); 
+    String c = "car";
+    String v = "truck";
+    if (c == v){
+        return true;
     } else{
-        System.out.println("Not Equal"); 
+        return false;
     }
+    
     }
     //Question 25
-  public void (ArrayList <> temp){
-        int i = 0;
+ /*public void findHighestTemp (ArrayList<Double> temperatures){
+    int count = 0;
+    double max = 0;
+    for (int i = 0; i > Collections.max; i++){
+        System.out.println("Highest case of fever: " + max);
+        
+    }
+    System.out.println("Number of fever cases reported: " + count);
+    
+        /*int i = 0;
         while(i >temp){
             i++; 
         }
@@ -45,15 +62,17 @@ public class Practice_Questions
             System.out.println(i+"are greater than 37.5");
             System.out.println("Max:" +temp);
         }
+        */
         //Question 26 
-        for (int i = 0; i >temp && i< Collections.max; i++){
+       /* for (int i = 0; i >temp && i< Collections.max; i++){
              System.out.println("Max:" + i);
         }
         //Question 27
          for (int i = 0; i >temp && i< Collections.sort; i++){
              System.out.println("Max:" + i);
     }
+    */
 }
-*/
-}
-}
+
+
+
